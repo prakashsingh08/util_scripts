7af829cf820585c037eaab733832e58701f2f7cb
Prakash.S <prakash.s@Prakashs-MP.local>
2018-02-08 15:05:07 +0530

    updating rows

--- cluster_details.sh
+++ cluster_details.sh
@@ -3,10 +3,10 @@
 array=(`gcloud container clusters  list --format="csv(name)" |awk '{if(NR>1)print}'`)
 array2=(`gcloud container clusters  list --format="csv(zone)" |awk '{if(NR>1)print}'`)
 
-
+echo "name,enabled,max_node_count,min_node_count"
 for index in ${!array[*]}; do
 
-gcloud container clusters describe ${array[$index]} --zone ${array2[$index]} --format="csv(name,nodePools.autoscaling.enabled,nodePools.autoscaling.maxNodeCount,nodePools.autoscaling.minNodeCount)"
+gcloud container clusters describe ${array[$index]} --zone ${array2[$index]} --format="csv(name,nodePools.autoscaling.enabled,nodePools.autoscaling.maxNodeCount,nodePools.autoscaling.minNodeCount)" |awk '{if(NR>1)print}'
 
 done
 
