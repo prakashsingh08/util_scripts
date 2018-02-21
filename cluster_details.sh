919e9107644584768e7d68664b7c7bae3b2e8790
Prakash.S <prakash.s@Prakashs-MP.local>
2018-02-21 08:44:52 +0530

    new updates

--- cluster_details.sh
+++ cluster_details.sh
@@ -8,5 +8,10 @@ for index in ${!array[*]}; do
 
 gcloud container clusters describe ${array[$index]} --zone ${array2[$index]} --format="csv(name,nodePools.autoscaling.enabled,nodePools.autoscaling.maxNodeCount,nodePools.autoscaling.minNodeCount)" |awk '{if(NR>1)print}'
 
+gcloud beta container clusters get-credentials ${array[$index]} --zone ${array2[$index]} --project kohls-mobile-hle > /dev/null 2>&1
+node_count=`kubectl get no |wc -l`
+echo "CURRENT_NODE_COUNT:$node_count"
+kubectl get no
+
 done
 
--- cpuinfo.sh
+++ cpuinfo.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #gcloud compute instances describe dm-lle-mongo --zone us-central1-a --project kohls-mobile-lle 
 a=(`kubectl get ns -o=custom-columns=NAME:.metadata.name`)
+
 for name_space in "${a[@]:1}"
 do
  # echo "NameSpace: $name_space "
